23848fc5ef0cc1cd4ab6c2ee5f97af959dbc66a2
KeoFoxy <arasdiel68@gmail.com>
2023-08-25 01:26:12 +0300

    Index.ts that includes export all components

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from './Hero/Hero';
+import CustomButton from './CustomButton/CustomButton';
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
