89e604b9b7c260586b0bff616fcac7c1184cd73a
KeoFoxy <arasdiel68@gmail.com>
2023-08-25 18:55:59 +0300

    Custom Buttons new icons, text styles and isDisabled

--- components/CustomButton/CustomButton.tsx
+++ components/CustomButton/CustomButton.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import {CustomButtonProps} from "@/components/CustomButton/CustomButton.props";
 
 
-const CustomButton = ({title, containerStyles, handleClick, buttonType}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, buttonType, textStyles, icon, isDisabled }: CustomButtonProps) => {
     return (
         <button
             disabled={false}
@@ -13,8 +13,15 @@ const CustomButton = ({title, containerStyles, handleClick, buttonType}: CustomB
             onClick={handleClick}
         >
             <span className={`flex-1`}>
-                {title}
+                <p className={textStyles}>
+                    {title}
+                </p>
             </span>
+            {icon && (
+                <div className={"relative w-6 h-6"}>
+                    <Image src={icon} alt={"icon"} fill className={"object-contain"}/>
+                </div>
+            )}
 
         </button>
     )
