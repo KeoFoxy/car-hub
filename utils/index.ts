4cf057ab02107570d4f15bcb7a1b56f8237e73ff
KeoFoxy <arasdiel68@gmail.com>
2023-08-26 01:07:23 +0300

    get car's image from API function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-
+import { CarProps } from "@/components/CarCard/Car.props";
 
 export async function fetchCars() {
     const headers = {
@@ -6,7 +6,7 @@ export async function fetchCars() {
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=cayenne', {
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
         headers: headers,
     });
 
@@ -15,7 +15,7 @@ export async function fetchCars() {
     return result;
 }
 
-export const calculateCarRent = (city_mpg: number, year: number): number => {
+export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
@@ -29,3 +29,18 @@ export const calculateCarRent = (city_mpg: number, year: number): number => {
 
     return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+}
