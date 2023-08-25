efbfc7efcb3967ce569744d4e303f2aab69486e7
KeoFoxy <arasdiel68@gmail.com>
2023-08-25 12:26:43 +0300

    Import of new components

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from './Hero/Hero';
 import CustomButton from './CustomButton/CustomButton';
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
+import SearchBar from "./SearchBar/SearchBar";
+import CustomFilter from "./CustomFilter/CustomFilter";
+import SearchManufacturer from "./SearchManufacturer/SearchManufacturer";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
