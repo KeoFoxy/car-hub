c6868ef80c3292be269a64457ea8523a67c72270
KeoFoxy <arasdiel68@gmail.com>
2023-08-26 22:17:49 +0300

    Update search

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { SearchManufacturer } from "@/components";
 
@@ -17,17 +18,34 @@ const SearchBar = () => {
 
     const [manufacturer, setManufacturer] = useState('');
     const [model, setModel] = useState('');
+    const router = useRouter();
     
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         if(manufacturer === '' && model === '') return alert('Please fill in the search bar');
 
-
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
     }
 
-    const updateSearchParams = (mode: string, manufacturer: string) => {
+    const updateSearchParams = (model: string, manufacturer: string) => {
+        const searchParams = new URLSearchParams(window.location.search);
 
+        if(model) {
+            searchParams.set('model', model);
+        } else {
+            searchParams.delete('model');
+        }
+
+        if(manufacturer) {
+            searchParams.set('manufacturer', manufacturer);
+        } else {
+            searchParams.delete('manufacturer');
+        }
+
+        const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+        router.push(newPathName, {scroll: false});
     }
 
     return (
