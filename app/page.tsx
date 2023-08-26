2805d815d945b2d8f53d946377ace0192d61b624
KeoFoxy <arasdiel68@gmail.com>
2023-08-26 22:18:15 +0300

    SSR fetching new cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
-import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { HomeProps } from "@/utils/Home.props";
+import { fuels, yearsOfProduction } from "@/constants";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "Porsche",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -20,8 +27,8 @@ export default async function Home() {
             <SearchBar />
 
             <div className={"home__filter-container"}>
-              <CustomFilter title={"fuel"} />
-              <CustomFilter title={"year"} />
+              <CustomFilter title={"fuel"} options={fuels}/>
+              <CustomFilter title={"year"} options={yearsOfProduction}/>
             </div>
           </div>
 
